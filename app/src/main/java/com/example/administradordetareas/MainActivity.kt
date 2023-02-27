40a1d81c0f986297a7a629527b6a790930eea319
Raúl Prieto García <raprieto@unirioja.es>
2023-02-27 16:31:38 +0100

    fs

---
+++ app/src/main/java/com/example/administradordetareas/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.administradordetareas
+
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.administradordetareas.ui.theme.AdministradorDeTareasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdministradorDeTareasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    AdministradorDeTareas()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AdministradorDeTareas() {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment  = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        val imagen = painterResource(R.drawable.ic_task_completed)
+        Image(painter = imagen, contentDescription = null)
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AdministradorDeTareasPreview() {
+    AdministradorDeTareasTheme {
+        Surface{
+            AdministradorDeTareas()
+        }
+    }
+}
